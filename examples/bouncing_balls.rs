b6dbbb2c2e13d8470b0d9cd2f7c71b8d3517cbbe
Chris Juchem <chrisjuchem15@gmail.com>
2023-01-26 22:10:52 -0500

    Balls example without indexes

---
+++ examples/bouncing_balls.rs
@@ -0,0 +1,160 @@
+use bevy::asset::Asset;
+use bevy::math::Vec3Swizzles;
+use bevy::prelude::*;
+use bevy::sprite::{Material2d, MaterialMesh2dBundle, Mesh2dHandle};
+use bevy::utils::HashMap;
+use rand::rngs::ThreadRng;
+use rand::{random, seq::IteratorRandom, thread_rng, Rng};
+use std::f32::consts::PI;
+
+const N_BALLS: usize = 1000;
+const MAX_WIDTH: f32 = 640.;
+const MAX_HEIGHT: f32 = 360.;
+
+#[derive(Component)]
+struct Velocity(Vec2);
+#[derive(Component)]
+struct Size(f32);
+
+// struct RegionIndex;
+#[derive(Debug, PartialEq, Eq)]
+enum Region {
+    TopLeft,
+    TopCenter,
+    TopRight,
+    CenterLeft,
+    CenterCenter,
+    CenterRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight,
+}
+
+fn get_region(v: Vec2) -> Region {
+    match (v.x / MAX_WIDTH, v.y / MAX_HEIGHT) {
+        (x, y) if x < -0.33 && y < -0.33 => Region::BottomLeft,
+        (x, y) if x < -0.33 && y > 0.33 => Region::TopLeft,
+        (x, _) if x < -0.33 => Region::CenterLeft,
+        (x, y) if x > 0.33 && y < -0.33 => Region::BottomRight,
+        (x, y) if x > 0.33 && y > 0.33 => Region::TopRight,
+        (x, _) if x > 0.33 => Region::CenterRight,
+        (_, y) if y < -0.33 => Region::BottomCenter,
+        (_, y) if y > 0.33 => Region::TopCenter,
+        (_, _) => Region::CenterCenter,
+    }
+}
+
+fn random_asset<T>(assets: &Assets<T>, rng: &mut ThreadRng) -> Handle<T>
+where
+    T: Asset,
+{
+    let mut handle = Handle::weak(assets.ids().choose(rng).unwrap());
+    handle.make_strong(assets);
+    handle
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    for color in [
+        Color::AQUAMARINE,
+        Color::BISQUE,
+        Color::BLUE,
+        Color::CRIMSON,
+        Color::DARK_GRAY,
+        Color::DARK_GREEN,
+        Color::FUCHSIA,
+        Color::GOLD,
+        Color::INDIGO,
+        Color::PINK,
+        Color::SALMON,
+        Color::PURPLE,
+        Color::OLIVE,
+        Color::MAROON,
+        Color::GREEN,
+        Color::SILVER,
+        Color::VIOLET,
+        Color::WHITE,
+        Color::YELLOW_GREEN,
+        Color::ORANGE,
+    ] {
+        materials.add(ColorMaterial::from(color));
+    }
+
+    let size_range = 2..8;
+    let mut mesh_map = HashMap::new();
+    for x in size_range.clone() {
+        mesh_map.insert(x, meshes.add(shape::Circle::new(x as f32).into()));
+    }
+
+    let mut rng = thread_rng();
+    for _ in 0..N_BALLS {
+        let size = rng.gen_range(size_range.clone());
+
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: mesh_map.get(&size).unwrap().clone().into(),
+                material: random_asset(&materials, &mut rng).into(),
+                transform: Default::default(),
+                ..default()
+            },
+            Velocity(Vec2::from_angle(rng.gen::<f32>() * 2. * PI) * (rng.gen::<f32>() * 3. + 0.5)),
+            Size(size as f32),
+        ));
+    }
+}
+
+fn move_balls(mut balls: Query<(&mut Transform, &Velocity)>) {
+    for (mut t, v) in &mut balls {
+        t.translation += Vec3::from((v.0, 0.));
+    }
+}
+
+fn bounce(mut balls: Query<(&Transform, &mut Velocity, &Size)>) {
+    for (t, mut v, s) in &mut balls {
+        if t.translation.x - s.0 < -MAX_WIDTH || t.translation.x + s.0 > MAX_WIDTH {
+            v.0.x *= -1.;
+        }
+        if t.translation.y - s.0 < -MAX_HEIGHT || t.translation.y + s.0 > MAX_HEIGHT {
+            v.0.y *= -1.;
+        }
+    }
+}
+
+fn update_colors(
+    index: Query<(Entity, &Transform)>,
+    // --
+    materials: Res<Assets<ColorMaterial>>,
+    click: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut commands: Commands,
+) {
+    if click.just_pressed(MouseButton::Left) {
+        if let Some(mut pos) = windows.get_primary().unwrap().cursor_position() {
+            pos.x -= MAX_WIDTH;
+            pos.y -= MAX_HEIGHT;
+            let cursor_region = get_region(pos);
+
+            let mat = random_asset(&materials, &mut thread_rng());
+            for (e, t) in &index {
+                if cursor_region == get_region(t.translation.xy()) {
+                    commands.entity(e).insert(mat.clone());
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(move_balls)
+        .add_system(bounce)
+        .add_system(update_colors)
+        .run();
+}
