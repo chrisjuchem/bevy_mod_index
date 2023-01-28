06911eca612272d9016f1136b4797b81d37f7616
Chris Juchem <chrisjuchem15@gmail.com>
2023-01-27 19:32:01 -0500

    Simplify SystemParam impl

--- src/index.rs
+++ src/index.rs
@@ -1,5 +1,6 @@
 use crate::unique_multimap::UniqueMultiMap;
-use bevy::ecs::system::{SystemChangeTick, SystemParam};
+use bevy::ecs::archetype::Archetype;
+use bevy::ecs::system::{ReadOnlySystemParam, SystemChangeTick, SystemMeta, SystemParam};
 use bevy::prelude::*;
 use bevy::utils::HashSet;
 use std::hash::Hash;
@@ -25,11 +26,21 @@ impl<I: IndexInfo> Default for IndexStorage<I> {
     }
 }
 
+type ChangedComponetsQuery<'w, 's, T> = Query<
+    'w,
+    's,
+    (
+        Entity,
+        // TODO: Figure out if the static lifetime is right here
+        &'static <T as IndexInfo>::Component,
+        ChangeTrackers<<T as IndexInfo>::Component>,
+    ),
+>;
+
 pub struct Index<'w, 's, T: IndexInfo + 'static> {
     storage: ResMut<'w, IndexStorage<T>>,
-    // TODO: Figure out if the static lifetime is right here
-    changes: Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>,
-    ticks: SystemChangeTick,
+    changes: ChangedComponetsQuery<'w, 's, T>,
+    current_tick: u32,
 }
 
 impl<'w, 's, T: IndexInfo> Index<'w, 's, T> {
@@ -39,112 +50,90 @@ impl<'w, 's, T: IndexInfo> Index<'w, 's, T> {
     }
 
     pub fn refresh(&mut self) {
-        if self.storage.last_refresh_tick >= self.ticks.change_tick() {
+        if self.storage.last_refresh_tick >= self.current_tick {
             return; // Already updated in this system.
         }
 
         for (entity, component, change_tracker) in &self.changes {
             if change_tracker
                 .ticks()
-                .is_changed(self.storage.last_refresh_tick, self.ticks.change_tick())
+                .is_changed(self.storage.last_refresh_tick, self.current_tick)
             {}
             self.storage.map.insert(&T::value(component), &entity);
         }
-        self.storage.last_refresh_tick = self.ticks.change_tick();
+        self.storage.last_refresh_tick = self.current_tick;
     }
 }
 
-pub struct FetchState<'w, 's, T: IndexInfo + 'static> {
-    state: (
-        <ResMut<'w, IndexStorage<T>> as bevy::ecs::system::SystemParam>::State,
-        <Query<
-            'w,
-            's,
-            (Entity, &'static T::Component, ChangeTrackers<T::Component>),
-        > as bevy::ecs::system::SystemParam>::State,
-        <SystemChangeTick as bevy::ecs::system::SystemParam>::State,
-    ),
-    marker: std::marker::PhantomData<
-        (
-            <bevy::ecs::prelude::Query<
-                'w,
-                's,
-                (),
-            > as bevy::ecs::system::SystemParam>::State,
-        ),
-    >,
+pub struct IndexFetchState<'w, 's, T: IndexInfo + 'static> {
+    storage_state: <ResMut<'w, IndexStorage<T>> as SystemParam>::State,
+    changed_components_state: <ChangedComponetsQuery<'w, 's, T> as SystemParam>::State,
 }
-unsafe impl<'w, 's, T: IndexInfo + 'static> bevy::ecs::system::SystemParam for Index<'w, 's, T> {
-    type State = FetchState<'static, 'static, T>;
+unsafe impl<'w, 's, T: IndexInfo + 'static> SystemParam for Index<'w, 's, T> {
+    type State = IndexFetchState<'static, 'static, T>;
     type Item<'_w, '_s> = Index<'_w, '_s, T>;
-    fn init_state(
-        world: &mut bevy::ecs::world::World,
-        system_meta: &mut bevy::ecs::system::SystemMeta,
-    ) -> Self::State {
-        FetchState {
-            state: <(
-                ResMut<'w, IndexStorage<T>>,
-                Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>,
-                SystemChangeTick,
-            ) as bevy::ecs::system::SystemParam>::init_state(world, system_meta),
-            marker: std::marker::PhantomData,
+    fn init_state(world: &mut World, system_meta: &mut SystemMeta) -> Self::State {
+        IndexFetchState {
+            storage_state: <ResMut<'w, IndexStorage<T>> as SystemParam>::init_state(
+                world,
+                system_meta,
+            ),
+            changed_components_state: <ChangedComponetsQuery<'w, 's, T> as SystemParam>::init_state(
+                world,
+                system_meta,
+            ),
         }
     }
-    fn new_archetype(
-        state: &mut Self::State,
-        archetype: &bevy::ecs::archetype::Archetype,
-        system_meta: &mut bevy::ecs::system::SystemMeta,
-    ) {
-        <(
-            ResMut<'w, IndexStorage<T>>,
-            Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>,
-            SystemChangeTick,
-        ) as bevy::ecs::system::SystemParam>::new_archetype(
-            &mut state.state,
+    fn new_archetype(state: &mut Self::State, archetype: &Archetype, system_meta: &mut SystemMeta) {
+        <ResMut<'w, IndexStorage<T>> as SystemParam>::new_archetype(
+            &mut state.storage_state,
             archetype,
             system_meta,
-        )
+        );
+        <ChangedComponetsQuery<'w, 's, T> as SystemParam>::new_archetype(
+            &mut state.changed_components_state,
+            archetype,
+            system_meta,
+        );
     }
-    fn apply(
-        state: &mut Self::State,
-        system_meta: &bevy::ecs::system::SystemMeta,
-        world: &mut bevy::ecs::world::World,
-    ) {
-        <(
-            ResMut<'w, IndexStorage<T>>,
-            Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>,
-            SystemChangeTick,
-        ) as bevy::ecs::system::SystemParam>::apply(&mut state.state, system_meta, world);
+    fn apply(state: &mut Self::State, system_meta: &SystemMeta, world: &mut World) {
+        <ResMut<'w, IndexStorage<T>> as SystemParam>::apply(
+            &mut state.storage_state,
+            system_meta,
+            world,
+        );
+        <ChangedComponetsQuery<'w, 's, T> as SystemParam>::apply(
+            &mut state.changed_components_state,
+            system_meta,
+            world,
+        );
     }
     unsafe fn get_param<'w2, 's2>(
         state: &'s2 mut Self::State,
-        system_meta: &bevy::ecs::system::SystemMeta,
-        world: &'w2 bevy::ecs::world::World,
+        system_meta: &SystemMeta,
+        world: &'w2 World,
         change_tick: u32,
     ) -> Self::Item<'w2, 's2> {
-        let (f0, f1, f2) = <(
-            ResMut<'w, IndexStorage<T>>,
-            Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>,
-            SystemChangeTick,
-        ) as bevy::ecs::system::SystemParam>::get_param(
-            &mut state.state,
-            system_meta,
-            world,
-            change_tick,
-        );
         Index {
-            storage: f0,
-            changes: f1,
-            ticks: f2,
+            storage: <ResMut<'w, IndexStorage<T>>>::get_param(
+                &mut state.storage_state,
+                system_meta,
+                world,
+                change_tick,
+            ),
+            changes: <ChangedComponetsQuery<'w, 's, T> as SystemParam>::get_param(
+                &mut state.changed_components_state,
+                system_meta,
+                world,
+                change_tick,
+            ),
+            current_tick: change_tick,
         }
     }
 }
-unsafe impl<'w, 's, T: IndexInfo + 'static> bevy::ecs::system::ReadOnlySystemParam
-    for Index<'w, 's, T>
+unsafe impl<'w, 's, T: IndexInfo + 'static> ReadOnlySystemParam for Index<'w, 's, T>
 where
-    ResMut<'w, IndexStorage<T>>: bevy::ecs::system::ReadOnlySystemParam,
-    Query<'w, 's, (Entity, &'static T::Component, ChangeTrackers<T::Component>)>:
-        bevy::ecs::system::ReadOnlySystemParam,
-    SystemChangeTick: bevy::ecs::system::ReadOnlySystemParam,
+    ResMut<'w, IndexStorage<T>>: ReadOnlySystemParam,
+    ChangedComponetsQuery<'w, 's, T>: ReadOnlySystemParam,
 {
 }
