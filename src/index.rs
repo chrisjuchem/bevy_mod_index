70b52143aecaf1e30c313657d77d812afc6969d8
Chris Juchem <chrisjuchem15@gmail.com>
2023-01-29 12:27:02 -0500

    Add some tests and fix a bug

--- src/index.rs
+++ src/index.rs
@@ -1,6 +1,6 @@
 use crate::unique_multimap::UniqueMultiMap;
 use bevy::ecs::archetype::Archetype;
-use bevy::ecs::system::{ReadOnlySystemParam, SystemChangeTick, SystemMeta, SystemParam};
+use bevy::ecs::system::{ReadOnlySystemParam, SystemMeta, SystemParam};
 use bevy::prelude::*;
 use bevy::utils::HashSet;
 use std::hash::Hash;
@@ -55,11 +55,16 @@ impl<'w, 's, T: IndexInfo> Index<'w, 's, T> {
         }
 
         for (entity, component, change_tracker) in &self.changes {
-            if change_tracker
-                .ticks()
-                .is_changed(self.storage.last_refresh_tick, self.current_tick)
-            {}
-            self.storage.map.insert(&T::value(component), &entity);
+            if change_tracker.ticks().is_changed(
+                // Subtract 1 so that changes from the system where the index was updated are seen.
+                // The `changed` implementation assumes we don't care about those changes since
+                // "this" system is the one that made the change, but for indexing, we do care.
+                self.storage.last_refresh_tick.wrapping_sub(1),
+                self.current_tick,
+            ) {
+                println!("update val for {:?}", entity);
+                self.storage.map.insert(&T::value(component), &entity);
+            }
         }
         self.storage.last_refresh_tick = self.current_tick;
     }
@@ -137,4 +142,105 @@ where
     ResMut<'w, IndexStorage<T>>: ReadOnlySystemParam,
     ChangedComponetsQuery<'w, 's, T>: ReadOnlySystemParam,
 {
+}
+
+mod test {
+    use crate::prelude::*;
+    use bevy::prelude::*;
+
+    #[derive(Component, Clone, Eq, Hash, PartialEq, Debug)]
+    struct Number(usize);
+
+    //todo: maybe make this a derive macro
+    impl IndexInfo for Number {
+        type Component = Self;
+        type Value = Self;
+
+        fn value(c: &Self::Component) -> Self::Value {
+            c.clone()
+        }
+    }
+
+    fn add_some_numbers(mut commands: Commands) {
+        commands.spawn(Number(10));
+        commands.spawn(Number(10));
+        commands.spawn(Number(20));
+        commands.spawn(Number(30));
+    }
+
+    fn checker(number: usize, amount: usize) -> impl Fn(Index<Number>) {
+        move |mut idx: Index<Number>| {
+            let set = idx.lookup(&Number(number));
+            assert_eq!(
+                set.len(),
+                amount,
+                "Index returned {} matches for {}, expectd {}.",
+                set.len(),
+                number,
+                amount,
+            );
+            println!("DONE CHEKING")
+        }
+    }
+
+    fn adder_all(n: usize) -> impl Fn(Query<&mut Number>) {
+        move |mut nums: Query<&mut Number>| {
+            for mut num in &mut nums {
+                num.0 += n;
+            }
+        }
+    }
+
+    fn adder_some(
+        n: usize,
+        condition: usize,
+    ) -> impl Fn(ParamSet<(Query<&mut Number>, Index<Number>)>) {
+        move |mut nums_and_index: ParamSet<(Query<&mut Number>, Index<Number>)>| {
+            for entity in nums_and_index.p1().lookup(&Number(condition)).into_iter() {
+                let mut nums = nums_and_index.p0();
+                let mut nref: Mut<Number> = nums.get_mut(entity).unwrap();
+                nref.0 += n;
+            }
+        }
+    }
+
+    #[test]
+    fn test_index_lookup() {
+        App::new()
+            .add_startup_system(add_some_numbers)
+            .add_system(checker(10, 2))
+            .add_system(checker(20, 1))
+            .add_system(checker(30, 1))
+            .add_system(checker(40, 0))
+            .run();
+    }
+
+    #[test]
+    fn test_changing_values() {
+        App::new()
+            .add_startup_system(add_some_numbers)
+            .add_system_to_stage(CoreStage::PreUpdate, checker(10, 2))
+            .add_system_to_stage(CoreStage::PreUpdate, checker(20, 1))
+            .add_system_to_stage(CoreStage::PreUpdate, checker(30, 1))
+            .add_system(adder_all(5))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(10, 0))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(20, 0))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(30, 0))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(15, 2))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(25, 1))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(35, 1))
+            .run();
+    }
+
+    #[test]
+    fn test_changing_with_index() {
+        App::new()
+            .add_startup_system(add_some_numbers)
+            .add_system_to_stage(CoreStage::PreUpdate, checker(10, 2))
+            .add_system_to_stage(CoreStage::PreUpdate, checker(20, 1))
+            .add_system(adder_some(10, 10))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(10, 0))
+            .add_system_to_stage(CoreStage::PostUpdate, checker(20, 3))
+            .run();
+    }
 }
